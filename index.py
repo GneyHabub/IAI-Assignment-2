c519fdf9f5ce8bb563294184b354d2886c931363
GneyHabub <31561808+GneyHabub@users.noreply.github.com>
2020-04-14 10:01:43 +0300

    ну шото уже есть

--- index.py
+++ index.py
@@ -1,91 +1,91 @@
 from PIL import Image, ImageDraw
-from numpy import zeros, uint8, cos, sin, array, argsort, where
+from numpy import zeros, uint8, cos, sin, array, argsort, where, delete
 from numpy.random import randint, uniform
 from numba import njit, prange
 from random import choice
+from sys import maxsize
 import math
-import config
+from colorthief import ColorThief as CT
+from time import time
 
 #config
+path = 'img/monaliza.jpg'
 populationSize = 35
 parentSize = 20
 childrenSize = 15
-lineLengths = [10, 15, 20, 30, 40, 50]
 populationFitness = []
+color_thief = CT(path)
+palette = array(color_thief.get_palette(color_count=10))
 
 # Evolutionary algorithm
-
-@njit(parallel=False)
-def generateImage():
-    res = zeros((512, 512, 3), dtype = uint8)
-    for i in range(512):
-        for j in range(512):
-            res[i, j, 0], res[i, j, 1], res[i, j, 2] = [236, 236, 228]
-    return res
-
 @njit(parallel=False)
 def generateCrossover(parents, parSize, chSize):
-    res = zeros((chSize, 512, 512, 3)) #initialize array with unsigned 8-bit integers
+    res = zeros((chSize, 512, 512, 3)) 
     for i in range(chSize):
-        parent1 = int((parSize - 1) * uniform(0, 1) ** 2)
-        parent2 = int((parSize - 1) * uniform(0, 1) ** 2)
-        res[i, 0:256] = parents[parent1, 0:256]
-        res[i, 256:] = parents[parent2, 256:]
+        res[i, 0:256] = parents[randint(parSize), 0:256]
+        res[i, 256:] = parents[randint(parSize), 256:]
     return res
 
 @njit(parallel=False)
 def mutate(population, popSize):
     for i in range(popSize):
-        # print("pop size:", i)
-        if uniform(0, 1) < 0.9:
-            population[i] = drawRandomLine(population[i])
+        ind = randint(len(palette))
+        col = palette[ind]
+        flag = randint(10)
+        if(flag < 9):
+            population[i] = drawRandomLine(population[i], col)
     return population
 
 @njit(parallel=True)
 def fitness(population, orig, popSize):
     fitness = zeros(len(population))
-    for x in range(len(population)):
-        distance = 0
-        for i in prange(population[x].shape[0]):
-            for j in range(population[x].shape[1]):
+    for item in range(len(population)):
+        fit = 0
+        for i in prange(512):
+            for j in range(512):
                 for k in range(3):
-                    distance += (int(population[x][i, j, k]) - int(orig[i, j, k])) ** 2
-        fitness[x] = distance
+                    fit += (int(population[item][i, j, k]) - int(orig[i, j, k])) ** 2
+        fitness[item] = fit
     return fitness
 
 @njit(parallel=True)
 def chooseParents(population, fit, parentSize):
     parents = zeros((parentSize, 512, 512, 3))
-    indices = argsort(populationFitness)
+    indices = argsort(fit)
     for i in prange(parentSize):
         parents[i] = population[indices[i]]
     return parents
 
 @njit(parallel=False)
-def drawRandomLine(canvas):
+def drawRandomLine(canvas, col):
     # print("Attempt to draw a random line")
-    length =  40 # lineLengths[randint(len(lineLengths))]
+    l = randint(10, 35)
     angle = math.radians(uniform(0, 360))
     x0 = randint(512)
     y0 = randint(512)
-    x1 = int(length * cos(angle) + x0)
-    y1 = int(length * sin(angle) + y0)
-    if(x1<x0):
-        x0, x1 = x1, x0
-    if(y1<y0):
-        y0, y1 = y1, y0
+    x1 = int(l * cos(angle) + x0)
+    y1 = int(l * sin(angle) + y0)
+    while (x1 == x0) or (y1==y0):
+        angle = math.radians(uniform(0, 360))
+        x1 = int(l * cos(angle) + x0)
+        y1 = int(l * sin(angle) + y0)   
+    # if(x1<x0):
+    #     x0, x1 = x1, x0
+    # if(y1<y0):
+    #     y0, y1 = y1, y0
     if (x1 > 512):
         x1 = 511
     if(y1 > 512):
         y1 = 511
     line = interpolate_pixels_along_line(x0, y0, x1, y1)
-    for i, j in line:
-        canvas[i, j] = [0, 0, 0]
+    for i in line:
+        canvas[i] = col
     # print("Succeeded at drawing a line")
     return canvas
 
 @njit(parallel=False)
 def interpolate_pixels_along_line(x0, y0, x1, y1):
+    # this function is taken form stackoverflow
     """Uses Xiaolin Wu's line algorithm to interpolate all of the pixels along a
     straight line, given two points (x0, y0) and (x1, y1)
     Wikipedia article containing pseudo code that function was based off of:
@@ -153,13 +153,37 @@ def interpolate_pixels_along_line(x0, y0, x1, y1):
 
     return pixels
 
+@njit(parallel=False)
+def get_average_color(x,y, n, image):
+    """ Returns a 3-tuple containing the RGB value of the average color of the
+    given square bounded area of length = n whose origin (top left corner) 
+    is (x, y) in the given image"""
+ 
+    r, g, b = 0, 0, 0
+    count = 0
+    for s in range(x, x+n+1):
+        for t in range(y, y+n+1):
+            if(len(image[s, t]) == 3):
+                pixlr, pixlg, pixlb = image[s, t]
+            else:
+                pixlr, pixlg, pixlb, chunk = image[s, t]
+            r += pixlr
+            g += pixlg
+            b += pixlb
+            count += 1
+    return ((r/count), (g/count), (b/count))
+
 # main body
-orig = array(Image.open('img/input2.jpg'))
+orig = array(Image.open(path))
+avgColor = get_average_color(0, 0, 512, orig)
+compColor = [255 - avgColor[0], 255- avgColor[1], 255- avgColor[2]]
+
 population = zeros((populationSize, 512, 512, 3), dtype = uint8) #initialize array with unsigned 8-bit integers
 for i in range(populationSize):
-    population[i] = generateImage() #get canvas
-
-chkpoint_fit = 0
+    population[i] = zeros(shape=(512, 512, 3), dtype = uint8)
+    for j in range(512):
+        for k in range(512):
+            population[i][j, k] = avgColor
 for i in range(100000):
     # print(i)
     populationFitness = fitness(population, orig, populationSize)
@@ -168,10 +192,9 @@ for i in range(100000):
     # print("passed parents")
     descendants = mutate(generateCrossover(parents, parentSize, childrenSize),childrenSize)
     # print("passed descend")
-    population[0:parents.shape[0], :] = parents
-    population[parents.shape[0]:, :] = descendants
+    population[0:20] = parents
+    population[20:] = descendants
     # print("passed population")
-    if abs(chkpoint_fit - min(populationFitness)) >= 100000:
-        chkpoint_fit = min(populationFitness)
+    if i % 100 == 0:
         top = where(populationFitness == min(populationFitness))
         Image.fromarray(population[top][0]).save('//Users/gneyhabub/Documents/GitHub/IAI-Assignment-2/img/output.jpg', 'JPEG')
